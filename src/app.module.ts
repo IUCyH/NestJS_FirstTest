8685684fab82e7802e78a32fcbd28b368382bfd0
Jaemin <ghdckdwn8456@gmail.com>
2025-01-27 19:50:44 +0900

    [feat] 빠진 부분 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppDataSource } from "./configs/orm.config";
 import { UserModule } from "./modules/user.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
     imports: [
+        TypeOrmModule.forRoot(AppDataSource),
         UserModule
     ],
     controllers: [AppController],
---
+++ src/configs/orm.config.ts
@@ -0,0 +1,22 @@
+import { DataSource } from "typeorm";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const AppDataSource: TypeOrmModuleOptions = {
+    type: "mariadb",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "abc1234",
+    database: "nest_test",
+    synchronize: false,
+    logging: true,
+    namingStrategy: new SnakeNamingStrategy(),
+    extra: {
+        timezone: "Asia/Seoul",
+        dateStrings: true
+    },
+    entities: [__dirname + "../../dist/entities/**/*.js"],
+    subscribers: [],
+    migrations: []
+};
