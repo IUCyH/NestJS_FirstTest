2d65416e55b0fc46360e158c553c632dbb6978b6
Jaemin <ghdckdwn8456@gmail.com>
2025-01-28 11:56:28 +0900

    [feat] 루트 모듈에 auth module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppDataSource } from "./configs/orm.config";
 import { UserModule } from "./modules/user.module";
+import { AuthModule } from "./modules/auth.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { JwtModule } from "@nestjs/jwt";
@@ -13,7 +14,8 @@ import { JwtModule } from "@nestjs/jwt";
             secret: "Lg24_8TrFZ_wvKZ5N2Qj8J9VpXcTQoYmFqL1X9RYsMzlRmtvOaq6UHpQkE3VMTX",
             signOptions: { issuer: "IUCyH", expiresIn: "30m" }
         }),
-        UserModule
+        UserModule,
+        AuthModule
     ],
     controllers: [AppController],
     providers: [AppService],
